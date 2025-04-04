7527167dac0457d2359f9b251c0ba45f8e1b46fa
popeaura <popeahura@gmail.com>
2025-04-04 17:48:17 +0300

    My modified projects folder

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -13,19 +13,25 @@ function Price() {
 
       <label className="sidebar-label-container">
           <input type="radio" name="test2" />
-          <span className="checkmark"></span>All
+          <span className="checkmark"></span>$0 - %50
       </label>
 
       <label className="sidebar-label-container">
           <input type="radio" name="test2" />
-          <span className="checkmark"></span>All
+          <span className="checkmark"></span>$50 - $100
       </label>
 
       <label className="sidebar-label-container">
           <input type="radio" name="test2" />
-          <span className="checkmark"></span>All
+          <span className="checkmark"></span>$100 - $150
       </label>
 
+
+
+      <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>$150 - $200
+      </label>
       </h2>
     </div>
   )
