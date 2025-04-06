04f1ae26597889e61b392113fb1e5a9bce023ca9
popeaura <popeahura@gmail.com>
2025-04-06 17:18:40 +0300

    My modified projects folder

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -6,31 +6,7 @@ import "./Products.css";
   return (
   <>
   <section className="card-container">
-    <section className="card">
-    <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
-     alt="Shoe"
-     className="card-img"/>
-    <div className="card-details">
-      <h3 className="card-title">
-        <section className="card-reviews">
-        <AiFillStar className="rating-star"/>
-        <AiFillStar className="rating-star"/>
-        <AiFillStar className="rating-star"/>
-        <AiFillStar className="rating-star" />
-
-        <span className="total-reviews">4</span>
-        </section>
-        <section className="card-price">
-          <div className="price">
-            <del>$ 300</del> 200
-          </div>
-          <div className="bag">
-          <BsFillBagHeartFill className="bag-icon" /> 
-          </div>
-        </section>
-      </h3>
-    </div>
-    </section>
+   
     <section className="card">
     <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
      alt="Shoe"
