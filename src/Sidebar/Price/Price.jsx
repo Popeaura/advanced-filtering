de394c4daf74494eec64ac1f95bc8b3d0d57d9b4
popeaura <popeahura@gmail.com>
2025-04-11 20:51:24 +0300

    My modified projects folder

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -6,6 +6,9 @@ function Price() {
     <div className="ml">
       <h2 className="sidebar-title price-title">Price</h2>
       <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
     </div>
   )
 }
