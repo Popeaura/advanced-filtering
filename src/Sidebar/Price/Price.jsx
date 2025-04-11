798690eda0e875f842a73499612d4e8b56663b7e
popeaura <popeahura@gmail.com>
2025-04-11 20:48:36 +0300

    My modified projects folder

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -3,29 +3,7 @@ import "./Price.css"
 function Price() {
   return (
     <div className="ml">
-      <h2 className="sidebar-title price-title">Price
-
-    
-      <label className="sidebar-label-container">
-          <input type="radio" name="test2" />
-          <span className="checkmark"></span>$0 - %50
-      </label>
-
-      <label className="sidebar-label-container">
-          <input type="radio" name="test2" />
-          <span className="checkmark"></span>$50 - $100
-      </label>
-
-      <label className="sidebar-label-container">
-          <input type="radio" name="test2" />
-          <span className="checkmark"></span>$100 - $150
-      </label>
-
-      <label className="sidebar-label-container">
-          <input type="radio" name="test2" />
-          <span className="checkmark"></span>Over $150 
-      </label>
-      </h2>
+      <h2 className="sidebar-title price-title">Price</h2>
     </div>
   )
 }
