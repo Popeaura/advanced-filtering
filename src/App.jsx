9eab20ea175dd69e105f8c1128fa1a9ed726562c
popeaura <popeahura@gmail.com>
2025-04-13 16:08:43 +0300

    My modified projects folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Sidebar from "./Sidebar/Sidebar";
-
+import Card from "./components/Card";
 import Navigation from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
