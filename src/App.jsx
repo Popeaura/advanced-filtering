8c7bae26e9580200e68ac4ffb3a25b8e53e60169
popeaura <popeahura@gmail.com>
2025-04-13 16:37:52 +0300

    My modified projects folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,82 @@
-import Sidebar from "./Sidebar/Sidebar";
-import Card from "./components/Card";
-import Navigation from "./Navigation/Nav";
-import Products from "./Products/Products";
-import Recommended from "./Recommended/Recommended";
 import { useState } from "react";
 
-//-------Database-------
-import products from './db/data'
-import Category from "./Sidebar/Category/Category";
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import products from "./db/data";
+import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
+import Card from "./components/Card";
+import "./index.css";
 
 function App() {
-  const [selectedCategory, setselectedCategory] = useState(null)
-  const [query, setQuery] = useState("")
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
+  // ----------- Input Filter -----------
+  const [query, setQuery] = useState("");
 
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
 
-//------input filter------
-  const handleInputChange = event => {
-    setQuery(event.target.value)
-  }
-
-  const filteredItems = products.filter(product =>
-     product.title.toLowerCase().indexOf(query.toLowerCase())!== -1
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
   );
 
+  // ----------- Radio Filtering -----------
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
 
-  //------Radio filter------
-  const handleChange = event => {
-    setselectedCategory(event.target.value)
-  }
+  // ------------ Button Filtering -----------
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
 
-  //------Buttons filter ----
-  const handleClick = event =>{
-    setselectedCategory(event.target.value)
-  }
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
 
-
-  function filterData(products,selected,query){
-    let filtereProducts = products
-
-    //filtering input items
-    if (query){
-      filtereProducts = filteredItems
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
     }
 
-    //selected filter
-    if (selected){
-      filtereProducts = filtereProducts.filter(
-        ({ category,color,company,newPrice, title}) =>
-         category === selected || 
-         color === selected|| 
-         company  === selected ||
-         newPrice === selected||
-         title === selected);
+    // Applying selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
     }
 
-    return filtereProducts.map(({img,title,star,reviews,newPrice,prevPrice}) =>(
-      <Card
-        key = {Math.random()}
-        img = {img}
-        title = {title}
-        star = {star}
-        reviews = {reviews}
-        newPrice = {newPrice}
-        prevPrice = {prevPrice}
-      />    
-    ))
-    
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
   }
+
+  const result = filteredData(products, selectedCategory, query);
+
   return (
     <>
-      <Sidebar/>
-      <Navigation/>
-      <Recommended/>
-      <Products/>
+      <Sidebar handleChange={handleChange} />
+      <Navigation query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick} />
+      <Products result={result} />
     </>
-  )
+  );
 }
-  
-  export default App;
+
+export default App;
