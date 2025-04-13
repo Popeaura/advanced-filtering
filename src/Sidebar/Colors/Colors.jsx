344cf4e2808241dc3b642260a8bea4f1c115eb3b
popeaura <popeahura@gmail.com>
2025-04-13 15:11:24 +0300

    My modified projects folder

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -1,7 +1,37 @@
 const Colors = () => {
   return (
     <div>
-      <Input/>
+      <h2 className="sidebar-title color-title">Colors</h2>
+        <label className="sidebar-label-container color-title">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>All
+      </label>
+
+      <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Black
+      </label>
+
+      <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Blue
+      </label>
+
+      <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Red
+      </label>
+
+
+      <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Green 
+      </label>
+
+      <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>White
+      </label>
     </div>
   );
 }
