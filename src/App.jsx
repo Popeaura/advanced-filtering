cac9c366b558415691670bd183678d587bfb6ce6
popeaura <popeahura@gmail.com>
2025-04-13 15:32:17 +0300

    My modified projects folder

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import { useState } from "react";
 
-//Database
+//-------Database-------
 import products from './db/data'
 
 function App() {
   const [selectedCategory, setselectedCategory] = useState(null)
 
-//input filter
+//------input filter------
   const [query, setQuery] = useState("")
 
   const handleInputChange = event => {
@@ -21,6 +21,9 @@ function App() {
   const filteredItems = products.filter(product => product.title.
     toLowerCase().indexOf(query.toLowerCase()!== -1)
   );
+
+
+  //------Radio filter------
 
   return (
     <>
