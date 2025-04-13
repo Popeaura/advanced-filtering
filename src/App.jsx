c3ef143bf4fb2bb002aa3c34ed4cd1d88dfdb663
popeaura <popeahura@gmail.com>
2025-04-13 15:19:30 +0300

    My modified projects folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Sidebar from "./Sidebar/Sidebar";
+
 import Navigation from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
+import { useState } from "react";
 
 function App() {
+  const [selectedCategory, selectedCategory] = useState(null)
+
+
   return (
     <>
       <Sidebar/>
--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -4,7 +4,11 @@ const Colors = () => {
   return (
     <div>
       <h2 className="sidebar-title color-title">Colors</h2>
+    <Input/>   
     <Input/>
+    <Input/>
+    <Input/>
+
     </div>
   );
 }
