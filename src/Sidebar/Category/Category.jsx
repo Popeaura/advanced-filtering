60da8044ab8881ade4419dd26e1e735ddc0b418a
popeaura <popeahura@gmail.com>
2025-04-14 20:07:40 +0300

    My modified projects folder

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -7,10 +7,12 @@ function Category({ handleChange }) {
       <h2 className="sidebar-title">Category</h2>
 
       <div>
-        <label className="sidebar-label-container">
-          <input onChange={handleChange} type="radio" value="" name="test" />
-          <span className="checkmark"></span>All
-        </label>
+      <label className="sidebar-label-container">
+      <input onChange={handleChange} type="radio" value="" name="test2" />
+      <span className="checkmark all"></span>
+      All
+      </label>
+
         <Input
           handleChange={handleChange}
           value="sneakers"
