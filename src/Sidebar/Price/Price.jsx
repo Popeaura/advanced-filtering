f353a95a01e2743c3202a58852ca3443372636d9
popeaura <popeahura@gmail.com>
2025-04-14 19:29:53 +0300

    My modified projects folder

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -1,14 +1,11 @@
 import Input from "../../components/Input";
 import "./Price.css"
 
-function Price(h) {
+function Price(handleChange) {
   return (
     <div className="ml">
       <h2 className="sidebar-title price-title">Price</h2>
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/> 
+ 
     </div>
   )
 }
