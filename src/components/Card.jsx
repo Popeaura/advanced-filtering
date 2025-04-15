b090aac2ee76746c6b6448b1ac681950a48e93d3
popeaura <popeahura@gmail.com>
2025-04-15 19:48:16 +0300

    My modified projects folder

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card({img, title, star, reviews, prevPrice, newPrice }) {
   return (
     <div>
    <section className="card">
-    <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+    <img src=
      alt="Shoe"
      className="card-img"/>
     <div className="card-details">
