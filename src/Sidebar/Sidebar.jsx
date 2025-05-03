f40fe38e05b035a7c4c9647861479289aed423d0
popeaura <popeahura@gmail.com>
2025-05-03 12:16:37 +0300

    My modified projects folder

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,26 +1,21 @@
 import Category from "./Category/Category";
 import Price from "./Price/Price";
 import Colors from "./Colors/Colors";
-import "./Sidebar.css"
-import { BsCartFill } from "react-icons/bs";
+import "./Sidebar.css";
 
-
-function Sidebar(handleChange){
-  
-return (
-  <>
-  <section className="sidebar">
-    <div className="logo-container">
-        <h1><BsCartFill /></h1>
-    </div>
-
-    <Category handleChange = {handleChange}/>
-    <Price handleChange = {handleChange}/>
-    <Colors handleChange = {handleChange}/>
-
-  </section>
-  </>   
-)
-}
+const Sidebar = ({ handleChange }) => {
+  return (
+    <>
+      <section className="sidebar">
+        <div className="logo-container">
+          <h1>🛒</h1>
+        </div>
+        <Category handleChange={handleChange} />
+        <Price handleChange={handleChange} />
+        <Colors handleChange={handleChange} />
+      </section>
+    </>
+  );
+};
 
 export default Sidebar;
