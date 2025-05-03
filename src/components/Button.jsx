39122a844a514ddd51f7cab7a1c5e2332778f413
popeaura <popeahura@gmail.com>
2025-05-03 12:25:15 +0300

    My modified projects folder

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 function Button({onClickHandler,value, title}) {
-  return <button onClick={onClickHandler} value={value}></button>
+  return <button onClick={onClickHandler} value={value} className="btns"></button>
 }
 
 export default Button;
