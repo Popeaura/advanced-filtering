fb5b036a874b5de465f6317f2b848ded5549ddd7
popeaura <popeahura@gmail.com>
2025-05-03 12:33:13 +0300

    My modified projects folder

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,28 @@
-import { AiFillStar } from "react-icons/ai";
-import { BsFillBagHeartFill } from "react-icons/bs";
+import { BsFillBagFill } from "react-icons/bs";
 
-function Card({img, title, star, reviews, prevPrice, newPrice }) {
+const Card = ({ img, title, star, reviews, prevPrice, newPrice }) => {
   return (
-    <div>
-   <section className="card">
-    <img src= {img}
-     alt={title}
-     className="card-img"/>
-    <div className="card-details">
-      <h3 className="card-title">{title}</h3>
-        <section className="card-reviews">
-        {star}  {star} {star}  {star}
+    <>
+      <section className="card">
+        <img src={img} alt={title} className="card-img" />
+        <div className="card-details">
+          <h3 className="card-title">{title}</h3>
+          <section className="card-reviews">
+            {star} {star} {star} {star}
+            <span className="total-reviews">{reviews}</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>{prevPrice}</del> {newPrice}
+            </div>
+            <div className="bag">
+              <BsFillBagFill className="bag-icon" />
+            </div>
+          </section>
+        </div>
+      </section>
+    </>
+  );
+};
 
-        <span className="total-reviews">{reviews}</span>
-        </section>
-        <section className="card-price">
-          <div className="price">
-            <del>{prevPrice}</del>  {newPrice}
-          </div>
-          <div className="bag">
-          <BsFillBagHeartFill className="bag-icon" />{newPrice}
-          </div>
-        </section>
-    
-    </div>
-    </section>
-    </div>
-  )
-}
-
-export default  Card;
+export default Card;
