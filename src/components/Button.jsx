c599cd0bd9926025f110a0d30c830e4b16ec71ad
popeaura <popeahura@gmail.com>
2025-05-03 12:27:37 +0300

    My modified projects folder

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,9 @@
-function Button({onClickHandler,value, title}) {
-  return <button onClick={onClickHandler} value={value} className="btns">
-    {title}
-  </button>
-}
+const Button = ({ onClickHandler, value, title }) => {
+  return (
+    <button onClick={onClickHandler} value={value} className="btns">
+      {title}
+    </button>
+  );
+};
 
 export default Button;
