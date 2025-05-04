03a3a2f7b14bf6abcc8650c3efb3c8ef09308d5d
popeaura <popeahura@gmail.com>
2025-05-04 23:59:57 +0300

    My modified projects folder

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,7 @@ function App() {
   // ----------- Radio Filtering -----------
   const handleChange = (event) => {
     setSelectedCategory(event.target.value);
-  };
-
+  
   // ------------ Button Filtering -----------
   const handleClick = (event) => {
     setSelectedCategory(event.target.value);
