4a3d633750ab5f32bc43e7db4f11c4c72060a22c
popeaura <popeahura@gmail.com>
2025-05-04 23:58:25 +0300

    My modified projects folder

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Sidebar from "./Sidebar/Sidebar";
 import Card from "./components/Card";
 import "./index.css";
 
-function App
+function App() 
   const [selectedCategory, setSelectedCategory] = useState(null);
 
   // ----------- Input Filter -----------
